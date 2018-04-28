33bc553dc4d1908bb7bb7a722b655402202f20b1
anirudhambati <34373561+anirudhambati@users.noreply.github.com>
2018-04-28 14:58:54 +0530

    Update mainActivity.py

--- mainActivity.py
+++ mainActivity.py
@@ -28,40 +28,55 @@ elif 'news' in res or 'happening' in res or 'happenings' in res or 'going on' in
 elif res=='hello' or res=='hi' or res=='hello buddy' or res=='hi buddy':
     option2=['hi','hello dear', 'hello', 'namaste', 'namaskaar']
     opt=option2[random.randint(0,4)]
-    tts = gTTS(text=opt, lang='en-us')
-    tts.save("hel.mp3")
-    os.system("mpg321 hel.mp3")
+    functionset.sayout(opt)
+    #tts = gTTS(text=opt, lang='en-us')
+    #tts.save("hel.mp3")
+    #os.system("mpg321 hel.mp3")
     print(opt)
 elif 'how do you do' in res:
-    tts = gTTS(text='how do you do?', lang='en-us')
-    tts.save("hel.mp3")
-    os.system("mpg321 hel.mp3")
+    opt='how do you do?'
+    functionset.sayout(opt)
+    #tts = gTTS(text='how do you do?', lang='en-us')
+    #tts.save("hel.mp3")
+    #os.system("mpg321 hel.mp3")
 elif 'what' in res and 'you' in res and ('doing' in res or 'feeling' in res):
     option1=['i am always intrested to learn new thing. would you like to hear to an intresting fact?','just having a laugh. would you like to listen to a joke?','i am bored.wondering what to do']
     opt=option1[random.randint(0,2)]
-    tts = gTTS(text=opt, lang='en-us')
-    tts.save("hel.mp3")
-    os.system("mpg321 hel.mp3")
+    functionset.sayout(opt)
+    #tts = gTTS(text=opt, lang='en-us')
+    #tts.save("hel.mp3")
+    #os.system("mpg321 hel.mp3")
 elif 'how are you' in res:
-    tts = gTTS(text='i am great. thanks for asking' or 'i am having a great day. thanks for asking', lang='en-us')
-    tts.save("hel.mp3")
-    os.system("mpg321 hel.mp3")
+    option1=['i am great. thanks for asking','i am having a great day. thanks for asking']
+    opt=option1[random.randint(0,2)]
+    functionset.sayout(opt)
+    #tts = gTTS(text='i am great. thanks for asking' or 'i am having a great day. thanks for asking', lang='en-us')
+    #tts.save("hel.mp3")
+    #os.system("mpg321 hel.mp3")
 elif 'I' in res and ('bored' in res or 'bore' in res):
-    tts = gTTS(text='here are the things i can do for you. play a game ,tell a joke, tell the news,and i can ask some riddles too', lang='en-us')
-    tts.save("hel.mp3")
-    os.system("mpg321 hel.mp3")
+    opt='I am bored too. shall we play a game?'
+    functionset.sayout(opt)
+    #tts = gTTS(text='here are the things i can do for you. play a game ,tell a joke, tell the news,and i can ask some riddles too', lang='en-us')
+    #tts.save("hel.mp3")
+    #os.system("mpg321 hel.mp3")
 elif 'what' in res and 'can' in res and 'you' in res and 'do' in res:
-    tts = gTTS(text='here are the things i can do for you. i can set alarms, remainder, create a shopping list or a to-do list for you, help chat with your friend, tell stories, play music , tell you the weather conditions, search for the prices of goods on flipkart, play a game ,tell a joke, tell the news,and i can ask some riddles too',lang='en-us')
-    tts.save("hel.mp3")
-    os.system("mpg321 hel.mp3")
+    opt='here are the things i can do for you. i can set alarms, remainder, create a shopping list or a to-do list for you, help chat with your friend, tell stories, play music , tell you the weather conditions, search for the prices of goods on flipkart, play a game ,tell a joke, tell the news,and i can ask some riddles too'
+    functionset.sayout(opt)
+    #tts = gTTS(text='here are the things i can do for you. i can set alarms, remainder, create a shopping list or a to-do list for you, help chat with your friend, tell stories, play music , tell you the weather conditions, search for the prices of goods on flipkart, play a game ,tell a joke, tell the news,and i can ask some riddles too',lang='en-us')
+    #tts.save("hel.mp3")
+    #os.system("mpg321 hel.mp3")
 elif ('who' in res or 'why' in res)and 'made' in res and 'you' in res:
-    tts = gTTS(text='i was made by the alasya group as a part of their ITWS2 software project ',lang='en-us')
-    tts.save("hel.mp3")
-    os.system("mpg321 hel.mp3")
+    opt='i was made by the alasya group as a part of their ITWS2 software project'
+    functionset.sayout(opt)
+    #tts = gTTS(text='i was made by the alasya group as a part of their ITWS2 software project ',lang='en-us')
+    #tts.save("hel.mp3")
+    #os.system("mpg321 hel.mp3")
 elif 'introduce' in res or 'introduction' in res:
-    tts = gTTS(text='hello! i am the voice assistant created by alasya group. you can call me "buddy". i can speak in 19 different languages ,but "dont" ask me to do it now!. here are the things i can do. i can set alarms, remainder, create a shopping list or a to-do list for you, help chat with your friend, tell stories, play music , tell you the weather conditions, search for the prices of goods on flipkart, play a game ,tell a joke, tell the news,and i can ask some riddles too ',lang='en-us')
-    tts.save("hel.mp3")
-    os.system("mpg321 hel.mp3")
+    opt='hello! i am the voice assistant created by alasya group. you can call me "buddy". here are the things i can do. i can set alarms, remainder, create a shopping list or a to-do list for you, help chat with your friend, tell stories, play music , tell you the weather conditions, search for the prices of goods on flipkart, play a game ,tell a joke, tell the news,and i can ask some riddles too '
+    functionset.sayout(opt)
+    #tts = gTTS(text='hello! i am the voice assistant created by alasya group. you can call me "buddy". here are the things i can do. i can set alarms, remainder, create a shopping list or a to-do list for you, help chat with your friend, tell stories, play music , tell you the weather conditions, search for the prices of goods on flipkart, play a game ,tell a joke, tell the news,and i can ask some riddles too ',lang='en-us')
+    #tts.save("hel.mp3")
+    #os.system("mpg321 hel.mp3")
 elif ('how old' in res and 'you' in res) or ('your age' in res and ('what is' in res or "what's" in res)) or ('when' in res and 'you' in res and 'born' in res):
     tts = gTTS(text='technically ,as i dont have intelligence , i was never born and do not have an age', lang='en-us')
     tts.save("hel.mp3")
